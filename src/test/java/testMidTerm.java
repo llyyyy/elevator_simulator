4b9513e234b5508603d192680cfe74d15af30f5f
KangSanghun <myks789@gmail.com>
2014-10-19 02:37:37 +0900

    중간고사 과제 수행 assertThat사용 커밋 첫번째

---
+++ src/test/java/testMidTerm.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by ksh on 2014-10-19.
+ */
+public class testMidTerm {
+    @Test
+    public void testMain(){
+        assertThat(1 + 2, is(3));
+    }
+
+
+}
